50cacadaa25f24241d1465d2477be00014530177
Sasidhar <sasidharon1142002@gmail.com>
2021-12-25 10:07:51 +0530

    popular shows

--- src/Components/Home/Homeshows.js
+++ src/Components/Home/Homeshows.js
@@ -1,19 +1,63 @@
-import React from 'react'
-import './Homeshows.css';
+import React from "react";
+import "./Homeshows.css";
+import show1 from "../../Assets/show1.jpg"
+import show2 from "../../Assets/show2.jpg"
+import show3 from "../../Assets/show3.jpg"
+import show4 from "../../Assets/show4.jpg"
+import show5 from "../../Assets/show5.jpg"
+import show6 from "../../Assets/show6.jpg"
+
 const Homeshows = () => {
-    return (
-        <div>
-            <div className='box'>
-                <div className='popshow'>Popular Shows</div>
-            <div>                
+  return (
+    <div>
+      <div className="box">
+        <div className="popshow">Popular Shows</div>
+        
+         <div className="show-container">
+          <div class="row">
+            <div class="column">
+              <img src={show1} width={253}/>
             </div>
-            <div className='show-container'>
-                
+            <div class="column">
+              <img src={show2} width={253}/>
             </div>
-        </div>
-        </div>
+            <div class="column">
+              <img src={show3} width={253}/>
+            </div>
+            <div class="column">
+              <img src={show4} width={253}/>
+            </div>
+            <div class="column">
+              <img src={show5} width={253}/>
+            </div>
+            <div class="column">
+              <img src={show6} width={253}/>
+            </div>
+          </div>
+          <div class="row">
+          <div class="column">
+              <img src={show1} width={253}/>
+            </div>
+            <div class="column">
+              <img src={show2} width={253}/>
+            </div>
+            <div class="column">
+              <img src={show3} width={253}/>
+            </div>
+            <div class="column">
+              <img src={show4} width={253}/>
+            </div>
+            <div class="column">
+              <img src={show5} width={253}/>
+            </div>
+            <div class="column">
+              <img src={show6} width={253}/>
+            </div>
+          </div>
+        </div> 
+      </div>
+    </div>
+  );
+};
 
-    )
-}
-
-export default Homeshows
+export default Homeshows;
